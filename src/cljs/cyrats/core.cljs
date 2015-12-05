235df392516688998404360ef8622f2782ae25a7
Aleksey <astynax84@gmail.com>
2015-12-05 07:40:07 +0300

    add navigation bar and some static

--- src/cljs/cyrats/core.cljs
+++ src/cljs/cyrats/core.cljs
@@ -3,17 +3,38 @@
             [quiescent.dom :as d]
             [cyrats.model :as m]))
 
-(defonce STATE (atom 42))
+(def NAVIGATION
+  [["index" ["[ About ]" "/"]]
+   ["rooms" ["[ Rooms ]" "/rooms"]]
+   ["profile" ["[ Profile ]" "/profile"]]])
 
-(q/defcomponent View [state]
-  (d/h1 {} state))
+(q/defcomponent NavItem
+  [current? title uri]
+  (d/a {:className (str "nav-item" (if current? " current" ""))
+        :href uri}
+       title))
 
-(let [root-el (.getElementById js/document "root")]
-  (defn- redraw [state]
-    (q/render (View state) root-el)))
+(q/defcomponent NavigationBar
+  [current-page]
+  (letfn [(rat-img [uri] (d/img {:className "rat-img"
+                                 :src uri}))]
+    (apply
+     d/div {:id "nav-bar"}
+     (rat-img "static/rat2.png")
+     (d/a {:className "logo"
+           :href "/"}
+          "CyRats")
+     (concat
+      (for [[page-name [title uri]] NAVIGATION]
+        (NavItem (= page-name current-page) title uri))
+      [(rat-img "static/rat1.png")]))))
 
-(add-watch STATE nil (fn [_ _ _ state]
-                       (redraw state)))
 
-(redraw @STATE)
+(defn ^:global do-navigation
+  [page]
+  (q/render (NavigationBar page)
+            (.getElementById js/document "navigation")))
 
+(defn ^:global do-content
+  [page]
+  )
